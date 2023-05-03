dbb36feb5707b7e81d0e4efc03e01095778471a8
ON22-SusanneZeilbeck <sus.zeilbeck.22@lehre.mosbach.dhbw.de>
2023-05-03 13:34:34 +0200

    DOM and interface index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-//THIS IS THE ENTRY FILE - WRITE YOUR MAIN LOGIC HERE!
+//DOM
+const newgoalInput = document.querySelector("#newgoalInput") as HTMLInputElement;
+const newgoalBtn = document.querySelector("#newgoalBtn") as HTMLButtonElement;
+const controlMessage = document.querySelector("#controlMessage") as HTMLSpanElement;
+const goalsContainer = document.querySelector("#goalsContainer") as HTMLDivElement;
+const locationElement = document.getElementById("location") as HTMLElement;
+const date_time_el = document.getElementById("date-time") as HTMLElement;
+//
+interface goal {
+  description: string;
+  id: string;
+  reached: boolean;
+  timestamp: Date;}
