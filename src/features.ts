2e3aefe90a65dd3415118752eb4dfa3375cd0ed0
ON22-SusanneZeilbeck <sus.zeilbeck.22@lehre.mosbach.dhbw.de>
2023-05-05 09:01:26 +0200

    add feature.ts for date/location and time

---
+++ src/features.ts
@@ -0,0 +1,25 @@
+import { date_time_el, locationElement } from "./index";
+
+ // Uhrzeit und Datum
+ function updateTime() {
+    const now = new Date();
+    const date_str = now.toLocaleDateString();
+    const time_str = now.toLocaleTimeString();
+    date_time_el.textContent = `${date_str}, ${time_str}`;}
+    setInterval(updateTime, 1000);
+  
+    //Standort
+    if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+    const { latitude, longitude } = position.coords;
+    fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`)
+    .then((response) => response.json())
+    .then((data) => {
+          const city = data.city;
+          const country = data.countryName;
+          locationElement.textContent = `My current location: ${city} ${country}`;})
+    .catch((error) => {
+          console.error(error);
+          locationElement.textContent = "failed to get your current location";});});}
+          else {locationElement.textContent = "geolocation is not supported by your browser";}
+  
--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ const newgoalInput = document.querySelector("#newgoalInput") as HTMLInputElement
 const newgoalBtn = document.querySelector("#newgoalBtn") as HTMLButtonElement;
 const controlMessage = document.querySelector("#controlMessage") as HTMLSpanElement;
 const goalsContainer = document.querySelector("#goalsContainer") as HTMLDivElement;
-const locationElement = document.getElementById("location") as HTMLElement;
-const date_time_el = document.getElementById("date-time") as HTMLElement;
+export const locationElement = document.getElementById("location") as HTMLElement;
+export const date_time_el = document.getElementById("date-time") as HTMLElement;
 //
 interface goal {
   description: string;
